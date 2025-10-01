376dfe89067e5a8e738552c787a65cb0f2d9e459
Mikhail Yakubov <licman@list.ru>
2025-10-01 16:34:06 +0400

    Revert "[add] homework for refactoring"
    This reverts commit c8fa68bf465bd546b5b6d628998ccb2b22852c41.

--- src/main/java/edu/t1/Default.java
+++
@@ -1,9 +0,0 @@
-package edu.t1;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Default {
-    Class value();
-}
